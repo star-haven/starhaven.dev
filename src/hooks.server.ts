2b30f5a908cb333ee9b2fdea5e76f5feecd1a8a8
Alex Bates <alex@nanaian.town>
2023-06-02 23:27:29 +0100

    simplify auth hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,29 +1,20 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Discord from "@auth/core/providers/discord";
-import { redirect, type Handle } from "@sveltejs/kit";
-import { sequence } from "@sveltejs/kit/hooks";
 import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from "$env/static/private";
 
-async function authorization({ event, resolve }) {
-    if (event.url.pathname.startsWith("/authenticated")) {
-        const session = await event.locals.getSession();
-        if (!session) {
-            throw redirect(303, "/login");
-        }
-    }
-
-    return resolve(event);
-}
-
-export const handle: Handle = sequence(
-    SvelteKitAuth({
-        providers: [
-            Discord({
-                clientId: process.env.DISCORD_CLIENT_ID || DISCORD_CLIENT_ID,
-                clientSecret:
-                    process.env.DISCORD_CLIENT_SECRET || DISCORD_CLIENT_SECRET,
-            }),
-        ],
-    }),
-    authorization,
-);
+export const handle = SvelteKitAuth({
+    providers: [
+        // auth.js's types seem to be broken
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        Discord({
+            clientId: process.env.DISCORD_CLIENT_ID || DISCORD_CLIENT_ID,
+            clientSecret:
+                process.env.DISCORD_CLIENT_SECRET || DISCORD_CLIENT_SECRET,
+        }),
+    ],
+    theme: {
+        colorScheme: "auto",
+        brandColor: "#ffd159",
+    },
+});
