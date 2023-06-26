65b63191f3091899ad7d58589b962d34d01aac34
KiritoDv <kiritodev01@gmail.com>
2023-06-26 12:48:41 -0600

    Changed type to use AssetData

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,12 +1,15 @@
 /* eslint-disable @typescript-eslint/ban-types */
 // Squelch warnings of image imports from your assets dir
+
+type AssetData = Object[];
+
 declare module "$lib/assets/*" {
-    const meta: Object[];
+    const meta: AssetData;
     export default meta;
 }
 
 // Added to remove warnings from boxArtUrl imports
 declare module "./*" {
-    const meta: Object[];
+    const meta: AssetData;
     export default meta;
 }
